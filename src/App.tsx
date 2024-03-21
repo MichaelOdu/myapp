05e8bfa804a0e555e1346f6ff03ef3ab2ded76e6
Oluwatobi Eluyemi <oeluy@Oyins-Macbook.local>
2024-03-20 21:47:50 -0400

    bring back

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter } from 'react-router-dom';
+import AppRoutes from "./routes";
+
+function App() {
+  return (
+    <BrowserRouter>
+    <div className="App">
+      <AppRoutes/>
+
+    </div>
+  </BrowserRouter>
+   
+  );
+}
+
+export default App;
---
+++ src/components/form.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+const Component = () => {
+
+
+    return (<>
+    <Container fluid>
+        Hello World Form
+    </Container>
+    </>);
+
+}
+
+export default Component;
